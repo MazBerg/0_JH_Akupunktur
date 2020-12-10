57c8b38169f6857c62da535fd51c821edcb2faf6
Maz Berg <Maz@MazBerg.com>
2020-12-10 20:28:00 +0100

    implement testimonials

--- main-js.js
+++ main-js.js
@@ -12,10 +12,13 @@ const btnAOneTitle = document.querySelector(".btn-a-title-container")
 const btnARing = document.querySelector(".btn-a-ring-pg-one")
 
 const btnBOne = document.querySelector(".btn-b-container")
+const btnBOneTitle = document.querySelector(".btn-b-title-container")
 
 const btnCOne = document.querySelector(".btn-c-container")
+const btnCOneTitle = document.querySelector(".btn-c-title-container")
 
 const btnDOne = document.querySelector(".btn-d-container")
+const btnDOneTitle = document.querySelector(".btn-d-title-container")
 
 function calcBtnPos() {
     const windowWidth = window.innerWidth
@@ -38,6 +41,8 @@ function calcBtnPos() {
         btnBOne.style.display = "inline-flex"
         btnBOne.style.left = `${xCoBtnB}px`
         btnBOne.style.top = `${yCoBtnB}px`
+        btnBOneTitle.style.left = `${xCoBtnB}px`
+        btnBOneTitle.style.top = `${yCoBtnB}px`
 
         // Btn C coordinates
         xCoBtnC = (windowWidth / 2) - (windowWidth * 0.0144)
@@ -45,6 +50,9 @@ function calcBtnPos() {
         btnCOne.style.display = "inline-flex"
         btnCOne.style.left = `${xCoBtnC}px`
         btnCOne.style.top = `${yCoBtnC}px`
+        btnCOneTitle.style.left = `${xCoBtnC}px`
+        btnCOneTitle.style.top = `${yCoBtnC}px`
+        
 
         // Btn D coordinates
         xCoBtnD = (windowWidth * 0.2664) + (windowWidth / 2)
@@ -52,6 +60,8 @@ function calcBtnPos() {
         btnDOne.style.display = "inline-flex"
         btnDOne.style.left = `${xCoBtnD}px`
         btnDOne.style.top = `${yCoBtnD}px`
+        btnDOneTitle.style.left = `${xCoBtnD}px`
+        btnDOneTitle.style.top = `${yCoBtnD}px`
 
     } else if ( (windowWidth/windowHeight) <= 1.5) {
         // console.log("Full Height")
@@ -61,6 +71,8 @@ function calcBtnPos() {
         btnAOne.style.display = "inline-flex"
         btnAOne.style.left = `${xCoBtnA}px`
         btnAOne.style.top = `${yCoBtnA}px`
+        btnAOneTitle.style.left = `${xCoBtnA}px`
+        btnAOneTitle.style.top = `${yCoBtnA}px`
 
         // Btn B coordinates
         yCoBtnB = (windowHeight / 2) + (windowHeight * 0.115776844)
@@ -68,6 +80,8 @@ function calcBtnPos() {
         btnBOne.style.display = "inline-flex"
         btnBOne.style.left = `${xCoBtnB}px`
         btnBOne.style.top = `${yCoBtnB}px`
+        btnBOneTitle.style.left = `${xCoBtnB}px`
+        btnBOneTitle.style.top = `${yCoBtnB}px`
 
         // Btn C coordinates
         yCoBtnC = (windowHeight / 2) + (windowHeight * 0.26694661)
@@ -75,6 +89,8 @@ function calcBtnPos() {
         btnCOne.style.display = "inline-flex"
         btnCOne.style.left = `${xCoBtnC}px`
         btnCOne.style.top = `${yCoBtnC}px`
+        btnCOneTitle.style.left = `${xCoBtnC}px`
+        btnCOneTitle.style.top = `${yCoBtnC}px`
 
         // Btn D coordinates
         yCoBtnD = (windowHeight / 2) + (windowHeight * 0.065386922)
@@ -82,36 +98,57 @@ function calcBtnPos() {
         btnDOne.style.display = "inline-flex"
         btnDOne.style.left = `${xCoBtnD}px`
         btnDOne.style.top = `${yCoBtnD}px`
+        btnDOneTitle.style.left = `${xCoBtnD}px`
+        btnDOneTitle.style.top = `${yCoBtnD}px`
     }
 }
 
 calcBtnPos() // initilize button pos
 
-// btnAOne.addEventListener("mouseover", () => {
-//     btnAOneTitle.style.visibility = "visible"
-//     btnAOneTitle.classList.add("fade")
-//     btnAOneTitle.style.cursor = "pointer"
-// })
-
-// btnAOne.addEventListener("mouseout", () => {
-//     // btnAOneTitle.style.display = "none"
-//     btnAOneTitle.classList.remove("fade")
-//     btnAOneTitle.style.cursor = "default"
-// })
-
-// btnAOneTitle.addEventListener("mouseover", () => {
-//     // btnAOneTitle.style.display = "inline-flex"
-//     console.log(btnAOneTitle.style.opacity)
-//     if(btnAOneTitle.style.opacity !== ""){
-//         btnAOneTitle.classList.add("fade")
-//         btnAOneTitle.style.cursor = "pointer"
-//     }
-// })
-
-// btnAOneTitle.addEventListener("mouseout", () => {
-//     // btnAOneTitle.style.display = "none"
-//     btnAOneTitle.classList.remove("fade")
-//     btnAOneTitle.style.cursor = "default"
-// })
-
 window.onresize = calcBtnPos
+
+// Slider functionality
+const carousel = document.querySelector('.carousel');
+const slider = document.querySelector('.slider');
+
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let direction = -1;
+
+next.addEventListener('click', function() {
+    if (direction === 1) { // right
+        direction = -1;
+        // prepend currently visible child to the beginning so that way it will slide to the first element when we translate (-20%)
+        slider.prepend(slider.lastElementChild);
+    }
+
+    direction = -1;
+    carousel.style.justifyContent = 'flex-start';
+    slider.style.transform = 'translate(-20%)';  
+});
+
+prev.addEventListener('click', function() {
+    if (direction === -1) {
+        direction = 1;
+        slider.appendChild(slider.firstElementChild);
+    }
+    carousel.style.justifyContent = 'flex-end';    
+    slider.style.transform = 'translate(20%)';  
+  
+});
+
+slider.addEventListener('transitionend', function() {
+  // get the last element and append it to the front
+  
+    if (direction === 1) {
+        slider.prepend(slider.lastElementChild);
+    } else {
+        slider.appendChild(slider.firstElementChild);
+    }
+    
+    slider.style.transition = 'none';
+    slider.style.transform = 'translate(0)';
+    setTimeout(() => {
+        slider.style.transition = 'all 0.5s';
+    })
+}, false);
