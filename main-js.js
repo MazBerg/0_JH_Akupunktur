dda5ef4a2ec368cef543e218d51946edf21746e8
Maz Berg <Maz@MazBerg.com>
2020-12-15 12:51:38 +0100

    implement back to pageOne Btn

--- main-js.js
+++ main-js.js
@@ -1,3 +1,4 @@
+// Initializing x & y coordinates of buttons A-H
 let xCoBtnA = 0
 let yCoBtnA = 0
 let xCoBtnB = 0
@@ -6,10 +7,17 @@ let xCoBtnC = 0
 let yCoBtnC = 0
 let xCoBtnD = 0
 let yCoBtnD = 0
+let xCoBtnE = 0
+let yCoBtnE = 0
+let xCoBtnF = 0
+let yCoBtnF = 0
+let xCoBtnG = 0
+let yCoBtnG = 0
+let xCoBtnH = 0
+let yCoBtnH = 0
 
 const btnAOne = document.querySelector(".btn-a-container")
 const btnAOneTitle = document.querySelector(".btn-a-title-container")
-const btnARing = document.querySelector(".btn-a-ring-pg-one")
 
 const btnBOne = document.querySelector(".btn-b-container")
 const btnBOneTitle = document.querySelector(".btn-b-title-container")
@@ -19,6 +27,20 @@ const btnCOneTitle = document.querySelector(".btn-c-title-container")
 
 const btnDOne = document.querySelector(".btn-d-container")
 const btnDOneTitle = document.querySelector(".btn-d-title-container")
+
+const btnEOne = document.querySelector(".btn-e-container")
+const btnEOneTitle = document.querySelector(".btn-e-title-container")
+
+const btnFOne = document.querySelector(".btn-f-container")
+const btnFOneTitle = document.querySelector(".btn-f-title-container")
+
+const btnGOne = document.querySelector(".btn-g-container")
+const btnGOneTitle = document.querySelector(".btn-g-title-container")
+
+const btnHOne = document.querySelector(".btn-h-container")
+const btnHOneTitle = document.querySelector(".btn-h-title-container")
+
+const btnBack = document.querySelector(".back-btn")
 
 function calcBtnPos() {
     const windowWidth = window.innerWidth
@@ -63,6 +85,42 @@ function calcBtnPos() {
         btnDOneTitle.style.left = `${xCoBtnD}px`
         btnDOneTitle.style.top = `${yCoBtnD}px`
 
+        // Btn E coordinates
+        xCoBtnE = (windowWidth * 0.1568) + (windowWidth / 2)
+        yCoBtnE = (windowHeight / 2) - ((windowWidth * 0.1568) * 1.053571429)
+        btnEOne.style.display = "inline-flex"
+        btnEOne.style.left = `${xCoBtnE}px`
+        btnEOne.style.top = `${yCoBtnE}px`
+        btnEOneTitle.style.left = `${xCoBtnE}px`
+        btnEOneTitle.style.top = `${yCoBtnE}px`
+
+        // Btn F coordinates
+        xCoBtnF = (windowWidth * 0.1664) + (windowWidth / 2)
+        yCoBtnF = (windowHeight / 2) - ((windowWidth * 0.1664) * 0.09375)
+        btnFOne.style.display = "inline-flex"
+        btnFOne.style.left = `${xCoBtnF}px`
+        btnFOne.style.top = `${yCoBtnF}px`
+        btnFOneTitle.style.left = `${xCoBtnF}px`
+        btnFOneTitle.style.top = `${yCoBtnF}px`
+
+        // Btn G coordinates
+        xCoBtnG = (windowWidth * 0.136) + (windowWidth / 2)
+        yCoBtnG = (windowHeight / 2) + ((windowWidth * 0.136) * 1.267647059)
+        btnGOne.style.display = "inline-flex"
+        btnGOne.style.left = `${xCoBtnG}px`
+        btnGOne.style.top = `${yCoBtnG}px`
+        btnGOneTitle.style.left = `${xCoBtnG}px`
+        btnGOneTitle.style.top = `${yCoBtnG}px`
+
+        // Btn H coordinates
+        xCoBtnH = (windowWidth * 0.264) + (windowWidth / 2)
+        yCoBtnH = (windowHeight / 2) - ((windowWidth * 0.264) * 0.339393939)
+        btnHOne.style.display = "inline-flex"
+        btnHOne.style.left = `${xCoBtnH}px`
+        btnHOne.style.top = `${yCoBtnH}px`
+        btnHOneTitle.style.left = `${xCoBtnH}px`
+        btnHOneTitle.style.top = `${yCoBtnH}px`
+
     } else if ( (windowWidth/windowHeight) <= 1.5) {
         // console.log("Full Height")
         // Btn A coordinates
@@ -100,12 +158,80 @@ function calcBtnPos() {
         btnDOne.style.top = `${yCoBtnD}px`
         btnDOneTitle.style.left = `${xCoBtnD}px`
         btnDOneTitle.style.top = `${yCoBtnD}px`
+
+        // Btn E coordinates
+        yCoBtnE = (windowHeight / 2) - (windowHeight * 0.247750449)
+        xCoBtnE = (windowWidth / 2) + ( (windowHeight * 0.247750449) * 0.949152542)
+        btnEOne.style.display = "inline-flex"
+        btnEOne.style.left = `${xCoBtnE}px`
+        btnEOne.style.top = `${yCoBtnE}px`
+        btnEOneTitle.style.left = `${xCoBtnE}px`
+        btnEOneTitle.style.top = `${yCoBtnE}px`
+
+        // Btn F coordinates
+        yCoBtnF = (windowHeight / 2) - (windowHeight * 0.02339532)
+        xCoBtnF = (windowWidth / 2) + ( (windowHeight * 0.02339532) * 10.66666667)
+        btnFOne.style.display = "inline-flex"
+        btnFOne.style.left = `${xCoBtnF}px`
+        btnFOne.style.top = `${yCoBtnF}px`
+        btnFOneTitle.style.left = `${xCoBtnF}px`
+        btnFOneTitle.style.top = `${yCoBtnF}px`
+
+        // Btn G coordinates
+        yCoBtnG = (windowHeight / 2) + (windowHeight * 0.25854829)
+        xCoBtnG = (windowWidth / 2) + ( (windowHeight * 0.25854829) * 0.788863109)
+        btnGOne.style.display = "inline-flex"
+        btnGOne.style.left = `${xCoBtnG}px`
+        btnGOne.style.top = `${yCoBtnG}px`
+        btnGOneTitle.style.left = `${xCoBtnG}px`
+        btnGOneTitle.style.top = `${yCoBtnG}px`
+
+        // Btn H coordinates
+        yCoBtnH = (windowHeight / 2) - (windowHeight * 0.134373125)
+        xCoBtnH = (windowWidth / 2) + ( (windowHeight * 0.134373125) * 2.946428571)
+        btnHOne.style.display = "inline-flex"
+        btnHOne.style.left = `${xCoBtnH}px`
+        btnHOne.style.top = `${yCoBtnH}px`
+        btnHOneTitle.style.left = `${xCoBtnH}px`
+        btnHOneTitle.style.top = `${yCoBtnH}px`
     }
 }
 
 calcBtnPos() // initilize button pos
 
 window.onresize = calcBtnPos
+
+// Button functionality
+function showWelcome(){
+    const welcomeContent = document.querySelector('.content-container')
+    welcomeContent.classList.add("selected")
+    const clearBtn = document.querySelector('.content-container .inside-content__clear')
+    clearBtn.addEventListener("click", () => {
+        welcomeContent.classList.remove("selected")
+    })
+}
+btnAOne.addEventListener("click", showWelcome)
+btnAOneTitle.addEventListener("click", showWelcome)
+
+function showPageTwo(){
+    const pageOne = document.querySelector('.bg-image.page-one')
+    const pageTwo = document.querySelector('.bg-image.page-two')
+    pageOne.classList.remove("is-active")
+    pageTwo.classList.add("is-active")
+}
+
+btnBOne.addEventListener("click", showPageTwo)
+btnBOneTitle.addEventListener("click", showPageTwo)
+
+function showPageOne(){
+    const pageOne = document.querySelector('.bg-image.page-one')
+    const pageTwo = document.querySelector('.bg-image.page-two')
+    pageTwo.classList.remove("is-active")
+    pageOne.classList.add("is-active")
+}
+
+btnBack.addEventListener("click", showPageOne)
+
 
 // Slider functionality
 const carousel = document.querySelector('.carousel');
